13aceb1872e499fe1136a8dd2336ef045fa75fb9
Rohit Saini <rohitsaini0444@gmail.com>
2026-05-08 23:47:27 +0530

    Signup api created

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
 const { adminAuth, userAuth } = require('./middlewares/auth');
+const User = require('./models/user');
 
 const connectDB = require('./config/database');
+
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(200).json({
+      message: "User created successfully",
+      user
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: "Error creating user",
+      error
+    })
+  }
+});
 
 
 connectDB().then(() => {
