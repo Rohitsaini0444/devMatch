7c352355945078a2a19ba2a2db5b14d4e5e8a0f4
Rohit Saini <rohitsaini0444@gmail.com>
2026-05-09 13:41:41 +0530

    Api to update user

--- src/app.js
+++ src/app.js
@@ -81,6 +81,27 @@ app.delete('/user', async (req, res) => {
   }
 });
 
+// Update user by userId
+app.patch('/user', async (req, res) => {
+  try {
+    const dataToUpdate = req.body?.data;
+    const user = await User.findByIdAndUpdate({ _id: req.body?.userId }, dataToUpdate, { new: true });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      })
+    }
+    res.status(200).json({
+      message: "User updated successfully",
+      user
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: "Error updating user",
+      error
+    })
+  }
+});
 
 connectDB().then(() => {
   console.log("Database connection has been stablished");
