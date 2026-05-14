63aecf8842a3491aabd3168434f9aebcb0536b25
Rohit Saini <rohitsaini0444@gmail.com>
2026-05-14 12:45:52 +0530

    Login api added

--- src/app.js
+++ src/app.js
@@ -39,6 +39,33 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+//Login user
+app.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body?.email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      })
+    }
+    const isMatch = await bcrypt.compare(req.body?.password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      })
+    }
+    res.status(200).json({
+      message: "User logged in successfully",
+      user
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: "Error logging in user",
+      error
+    })
+  }
+});
+
 // get user by email
 app.get('/user', async (req, res) => {
   try {
